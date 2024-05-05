520abc3ddf191e7f3c12be5a50f452279015a2f0
STC <stc1988@users.noreply.github.com>
2024-05-05 16:46:43 +0900

    add WifiApConnectQRcodeApp

---
+++ examples/piu/wifiApConnectQRcode/main.js
@@ -0,0 +1,30 @@
+import {} from "piu/MC";
+import WiFi from "wifi";
+import QrCodeScreen from "qrcodeScreen";
+
+class WifiApConnectQRcodeBehavior extends Behavior {
+  onCreate(application, data) {
+    const AP_NAME = "SettingAP";
+    const AP_PASSWORD = "12345678";
+    WiFi.accessPoint({
+      ssid: AP_NAME,
+      password: AP_PASSWORD,
+    });
+    const qrString = `WIFI:S:${AP_NAME};T:WPA;P:${AP_PASSWORD};;`;
+
+    application.add(
+      new QrCodeScreen({
+        string: qrString,
+        labels: ["Wifi-setting", "Scan QR Code"],
+      })
+    );
+  }
+}
+
+const WifiApConnectQRcodeApp = Application.template(($) => ({
+  Behavior: WifiApConnectQRcodeBehavior,
+}));
+
+export default function () {
+  return new WifiApConnectQRcodeApp();
+}
