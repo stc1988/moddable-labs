4c7b16059c3f482bc3c2bfb051f4207de343fc14
STC <stc1988@users.noreply.github.com>
2024-05-21 22:54:16 +0900

    add ollama/chat-stream

---
+++ examples/network/services/ollama/chat-stream/main.js
@@ -0,0 +1,60 @@
+import { fetch, Headers } from "fetch";
+import config from "mc/config";
+
+function completions(host, model, content) {
+  return new ReadableStream({
+    async start(controller) {
+      try {
+        const response = await fetch(`${host}/api/chat`, {
+          method: "POST",
+          headers: new Headers([["Content-Type", "application/json"]]),
+          body: JSON.stringify({
+            model,
+            stream: true,
+            messages: [{ role: "user", content: content }],
+          }),
+        });
+        const transformedBody = response.body.pipeThrough(
+          new TextDecoderStream()
+        );
+        const reader = transformedBody.getReader();
+
+        async function push() {
+          while (true) {
+            const { done, value } = await reader.read();
+            if (done) {
+              controller.close();
+              break;
+            }
+            const obj = JSON.parse(value, [
+              "message",
+              "role",
+              "content",
+              "done",
+            ]);
+            controller.enqueue(obj.message.content);
+          }
+        }
+
+        push().catch((error) => {
+          controller.error(error);
+        });
+      } catch (error) {
+        controller.error(error);
+      }
+    },
+  });
+}
+
+async function main() {
+  const stream = completions(
+    config.host,
+    "llama3",
+    "why is the sky blue in short?"
+  );
+  for await (const chunk of stream) {
+    trace(chunk || "");
+  }
+}
+
+main();
