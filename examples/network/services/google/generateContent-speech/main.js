d3658c96ed0e052950eb3bffe1badbfdc7ca22a2
STC <stc1988@users.noreply.github.com>
2024-05-23 23:18:01 +0900

    add google/generateContent-vision and google/generateContent-speech

---
+++ examples/network/services/google/generateContent-speech/main.js
@@ -0,0 +1,38 @@
+import config from "mc/config";
+import { fetch, Headers } from "fetch";
+import Resource from "Resource";
+
+// API specification: https://ai.google.dev/api/rest/v1beta/models/generateContent
+
+const apiKey = config.api_key;
+const model = "gemini-1.5-flash-latest";
+
+async function completions(body) {
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`,
+    {
+      method: "POST",
+      headers: new Headers([["Content-Type", "application/json"]]),
+      body: JSON.stringify(body),
+    }
+  );
+  const json = await response.json();
+  return json.candidates[0].content.parts[0].text;
+}
+const audio = new Uint8Array(new Resource("speech.wav"));
+const chatCompletion = await completions({
+  contents: [
+    {
+      parts: [
+        {
+          inlineData: {
+            mimeType: "audio/wav",
+            data: audio.toBase64(),
+          },
+        },
+      ],
+    },
+  ],
+});
+
+trace(`${chatCompletion}\n`);
