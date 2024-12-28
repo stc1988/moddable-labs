bcc4402873fe06e9a294eb8860ab565f161305ef
STC <stc1988@users.noreply.github.com>
2024-12-28 17:24:42 +0900

    add valibot

---
+++ examples/packages/valibot/types/lib-dom.d.ts
@@ -0,0 +1,16 @@
+
+interface Blob {
+    readonly size: number;
+    readonly type: string;
+    arrayBuffer(): Promise<ArrayBuffer>;
+    bytes(): Promise<Uint8Array>;
+    slice(start?: number, end?: number, contentType?: string): Blob;
+    stream(): any;
+    text(): Promise<string>;
+}
+
+interface File extends Blob {
+    readonly lastModified: number;
+    readonly name: string;
+    readonly webkitRelativePath: string;
+}
