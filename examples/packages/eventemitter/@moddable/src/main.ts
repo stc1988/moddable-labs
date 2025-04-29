0b95c18be3e05b19f4ba400c572ce6ca70f78f2e
STC <stc1988@users.noreply.github.com>
2025-04-29 22:40:57 +0900

    typed-emitter

--- examples/packages/eventemitter/@moddable/src/main.ts
+++ examples/packages/eventemitter/@moddable/src/main.ts
@@ -1,5 +1,15 @@
 import EventEmitter from "@moddable/eventemitter3";
+import TypedEmitter from "typed-emitter"
 
-const ee = new EventEmitter;
-ee.on("boo", arg => trace(`event "boo" with argument ${arg}\n`)); 
-ee.emit("boo", 2);
+type MessageEvents = {
+    error: (error: Error) => void,
+    message: (body: string, from: string) => void
+}
+
+const messageEmitter = new EventEmitter() as TypedEmitter<MessageEvents>
+
+messageEmitter.on("message", (body, from) => {
+    trace(`event "message" with body ${body} from ${from}\n`)
+});
+
+messageEmitter.emit("message", "hello", "world");
