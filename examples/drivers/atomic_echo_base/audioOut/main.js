11ff3288f3bc6384147d4e1d4d88676c266aeaa2
STC <stc1988@users.noreply.github.com>
2025-05-16 22:25:18 +0900

    atomi echo base (AudioOut only)

---
+++ examples/drivers/atomic_echo_base/audioOut/main.js
@@ -0,0 +1,36 @@
+// based on examples/io/audioout/play-sync
+
+import AudioOut from "embedded:io/audio/out";
+import {MAUD, SampleFormat} from "maudHeader";
+import Resource from "Resource";
+
+const one = loadSound("one.maud");
+const two = loadSound("two.maud");
+let playing = one;
+
+const output = new AudioOut({
+	onWritable(size) {
+		do {
+			let use = playing.byteLength - playing.position;
+			if (use > size) use = size;
+			this.write(playing.subarray(playing.position, playing.position + use));
+			playing.position += use;
+			if (playing.position === playing.byteLength) {
+				playing.position = 0;
+				playing = (playing === one) ? two : one;
+			}
+			size -= use;
+		} while (size);
+	}
+});
+output.start();
+
+function loadSound(name) {
+	let snd = new MAUD(new Resource(name));
+	if (!(1 === snd.version) && ("ma" === snd.tag) && (SampleFormat.Uncompressed === snd.sampleFormat) && (16 === snd.bitPerSample)  && (1 === snd.channels))
+		throw new Error;
+	snd = new Uint8Array(snd.buffer, snd.byteOffset + snd.byteLength, snd.bufferSamples * 2);
+	snd.position = 0;
+	
+	return snd;
+}
---
+++ examples/drivers/atomic_echo_base/conversationalAI/main.js
@@ -0,0 +1,17 @@
+import ChatAudioIO from "ChatAudioIO";
+
+const chat = new ChatAudioIO({
+	specifier: "openAIRealtime",
+	voiceName: "alloy",
+	instructions: "You're a hostile fisherman with a salty sense of humor. You dislike people and care even less for fish.",
+	onStateChanged(state) {
+		trace(`State: ${ChatAudioIO.states[state]} ${this.error ?? ""}\n`);
+	},
+	onInputTranscript(text) {
+		trace(`User: ${text}\n`);
+	},
+	onOutputTranscript(text) {
+		trace(`Agent: ${text}\n`);
+	},
+});
+chat.connect();
