26f030c2e28ba93b8a00338ac7a2e01391682369
STC <stc1988@users.noreply.github.com>
2026-04-29 10:21:31 +0900

    elevenLabs transcription

--- modules/network/services/transcription-realtime/main.js
+++ modules/network/services/transcription-realtime/main.js
@@ -1,12 +1,13 @@
 import ChatAudioIO from "ChatAudioIO";
 
 const chat = new ChatAudioIO({
-	specifier: "openAIRealtimeTranscription",
-	onStateChanged(state) {
-		trace(`State: ${ChatAudioIO.states[state]} ${this.error ?? ""}\n`);
-	},
-	onInputTranscript(text) {
-		trace(`User: ${text}\n`);
-	}
+  // specifier: "openAIRealtimeTranscription",
+  specifier: "elevenLabsRealtimeTranscription",
+  onStateChanged(state) {
+    trace(`State: ${ChatAudioIO.states[state]} ${this.error ?? ""}\n`);
+  },
+  onInputTranscript(text) {
+    trace(`User: ${text}\n`);
+  },
 });
 chat.connect();
